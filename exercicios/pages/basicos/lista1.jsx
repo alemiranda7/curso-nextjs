b4ae5c800a3b1a59e03ad05922bfed825158262d
alemiranda7 <7alemiranda@gmail.com>
2021-09-25 11:32:41 -0300

    Create lista1.jsx

---
+++ exercicios/pages/basicos/lista1.jsx
@@ -0,0 +1,16 @@
+export default function lista1(){
+    return (
+        <div>
+            <h1><span>1!</span></h1>
+            <h1><span>2!</span></h1>
+            <h1><span>3!</span></h1>
+            <h1><span>4!</span></h1>
+            <h1><span>5!</span></h1>
+            <h1><span>6!</span></h1>
+            <h1><span>7!</span></h1>
+            <h1><span>8!</span></h1>
+            <h1><span>9!</span></h1>
+            <h1><span>10!</span></h1>
+        </div>
+    )
+}
