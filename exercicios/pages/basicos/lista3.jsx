42766c3194e6f5a6df8aafe72dc7d68e56260c67
alemiranda7 <7alemiranda@gmail.com>
2021-09-26 17:12:11 -0300

    Update lista3.jsx

--- exercicios/pages/basicos/lista3.jsx
+++ exercicios/pages/basicos/lista3.jsx
@@ -1,34 +1,17 @@
-function gerarLista() {
+function gerarLista(final = 10) {
     const lista = []
-    for (let i = 0; i <= 10; i++){
-        lista.push(<span>{i}, </span>) 
+    for (let i = 0; i <= final; i++) {
+        lista.push(<span>{i}, </span>)
     }
 
     return lista
-
-    /*const lista = [
-        <div>
-            <span>0!</span>
-            <span>1!</span>
-            <span>2!</span>
-            <span>3!</span>
-            <span>4!</span>
-            <span>5!</span>
-            <span>6!</span>
-            <span>7!</span>
-            <span>8!</span>
-            <span>9!</span>
-        </div>
-    ]
-
-    return lista
-    */
 }
 
 export default function lista() {
     return (
         <div>
-            {gerarLista()}
+            <div>{gerarLista()}</div>
+            <div>{gerarLista(5)}</div>
         </div>
     )
 }
